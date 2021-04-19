e118114dc08931de4f520b7314c128009b3aee42
Nikohtr <Niko.htr@gmail.com>
2021-04-20 00:00:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -137,4 +137,6 @@ async def clear(ctx):
         pass
         
 token = os.getenv("DISCORD_BOT_SECRET2")
+token1 = os.getenv("DISCORD_BOT_SECRET2")
 client1.run(token, bot = False)
+client.run(token1)
