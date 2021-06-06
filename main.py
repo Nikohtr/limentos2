6e9fb10b61e49063ad86659986e26a4cfbe5abed
Nikohtr <Niko.htr@gmail.com>
2021-06-07 00:12:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ bot = discum.Client(token=t)
 def resptest(resp):
     if resp.event.message:
         if (resp.raw['d']['author']['id'] == '432610292342587392' or resp.raw['d']['author']['id'] == '780731609060999178')and resp.raw['d']['embeds'][0]['author']['name'].lower() in ch and ("React with any emoji to claim!" in resp.raw['d']['embeds'][0]['description'] or "Wished by" in resp.raw['d']['content']):
-            time.sleep(0.5)
+            time.sleep(0.3)
             bot.addReaction(resp.raw['d']['channel_id'],resp.raw['d']['id'], "👍")
 bot.gateway.run()
