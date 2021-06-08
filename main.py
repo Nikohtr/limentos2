2b32eef5cd15f0699d44a1b2fe2954a49a2e9dea
Nikohtr <Niko.htr@gmail.com>
2021-06-08 20:59:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import discum
 import os
-import time
 ch = ["zero two", "richard watterson"]
 t = os.getenv("discord_token")
 bot = discum.Client(token=t)
 @bot.gateway.command
-def resptest(resp):
+def respnowish(resp):
     if resp.event.message:
-        if (resp.raw['d']['author']['id'] == '432610292342587392' or resp.raw['d']['author']['id'] == '780731609060999178')and resp.raw['d']['embeds'][0]['author']['name'].lower() in ch and ("React with any emoji to claim!" in resp.raw['d']['embeds'][0]['description'] or "Wished by" in resp.raw['d']['content']):
-            time.sleep(0.2)
-            print(bot.getMessage(resp.raw['d']['channel_id'],resp.raw['d']['id']))
+        if (resp.raw['d']['author']['id'] == '432610292342587392' or resp.raw['d']['author']['id'] == '780731609060999178')and resp.raw['d']['embeds'][0]['author']['name'].lower() in ch and "React with any emoji to claim!" in resp.raw['d']['embeds'][0]['description']:
+            bot.addReaction(resp.raw['d']['channel_id'],resp.raw['d']['id'], "👍")
+def respwish(resp):
+    if resp.event.reaction_added:
+        msg = bot.getMessage(resp.raw['d']['channel_id'],resp.raw['d']['message_id'])
+        if (resp.raw['d']['author']['id'] == '432610292342587392' or resp.raw['d']['author']['id'] == '780731609060999178')and resp.raw['d']['embeds'][0]['author']['name'].lower() in ch and "Wished by" in resp.raw['d']['content']:
             bot.addReaction(resp.raw['d']['channel_id'],resp.raw['d']['id'], "👍")
 bot.gateway.run()
