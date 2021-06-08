c03e9162d309f10638e218d899510bbc41e8d15a
Nikohtr <Niko.htr@gmail.com>
2021-06-08 21:28:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ bot = discum.Client(token=t)
 @bot.gateway.command
 def respnowish(resp):
     if resp.event.message:
-        if (resp.raw['d']['author']['id'] == '432610292342587392' or resp.raw['d']['author']['id'] == '780731609060999178')and resp.raw['d']['embeds'][0]['author']['name'].lower() in ch and "React with any emoji to claim!" in resp.raw['d']['embeds'][0]['description']:
+        if (resp.raw['d']['author']['id'] == '432610292342587392' or resp.raw['d']['author']['id'] == '780731609060999178') and resp.raw['d']['embeds'][0]['author']['name'].lower() in ch and "React with any emoji to claim!" in resp.raw['d']['embeds'][0]['description']:
             bot.addReaction(resp.raw['d']['channel_id'],resp.raw['d']['id'], "👍")
     if resp.event.reaction_added:
         msg = bot.getMessage(resp.raw['d']['channel_id'],resp.raw['d']['message_id']).json()
-        print("\n\n\n\n\n\n\n\n\n\n\n", msg, "\n\n\n\n\n\n\n\n\n\n\n")
-        if (msg['d']['author']['id'] == '432610292342587392' or msg['d']['author']['id'] == '780731609060999178')and msg['d']['embeds'][0]['author']['name'].lower() in ch and "Wished by" in msg['d']['content']:
-            bot.addReaction(resp.raw['d']['channel_id'],resp.raw['d']['id'], "👍")
+        if (msg[0]['author']['id'] == '432610292342587392' or msg[0]['author']['id'] == '780731609060999178') and msg[0]['embeds'][0]['author']['name'].lower() in ch and "Wished by" in msg[0]['content']:
+            bot.addReaction(msg[0]['channel_id'],msg[0]['id'], "👍")
 bot.gateway.run()
