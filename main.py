acbb82122e43e432b18d469823c5d03224f8c189
Nikohtr <Niko.htr@gmail.com>
2021-06-08 21:18:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,9 @@ def respnowish(resp):
     if resp.event.message:
         if (resp.raw['d']['author']['id'] == '432610292342587392' or resp.raw['d']['author']['id'] == '780731609060999178')and resp.raw['d']['embeds'][0]['author']['name'].lower() in ch and "React with any emoji to claim!" in resp.raw['d']['embeds'][0]['description']:
             bot.addReaction(resp.raw['d']['channel_id'],resp.raw['d']['id'], "👍")
-
-@bot.gateway.command            
-def respwish(resp):
     if resp.event.reaction_added:
-        msg = bot.getMessage(resp.raw['d']['channel_id'],resp.raw['d']['message_id'])
-        print("\n\n\n\n\n\n\n\n\n\n\n", msg, msg.raw, "\n\n\n\n\n\n\n\n\n\n\n")
+        msg = bot.getMessage(resp.raw['d']['channel_id'],resp.raw['d']['message_id']).raw
+        print("\n\n\n\n\n\n\n\n\n\n\n", msg, "\n\n\n\n\n\n\n\n\n\n\n")
         if (msg['d']['author']['id'] == '432610292342587392' or msg['d']['author']['id'] == '780731609060999178')and msg['d']['embeds'][0]['author']['name'].lower() in ch and "Wished by" in msg['d']['content']:
             bot.addReaction(resp.raw['d']['channel_id'],resp.raw['d']['id'], "👍")
 bot.gateway.run()
